79dc5eda3d5494e62b6c77eba4f6f0da0e878f14
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-08-01 19:35:47 -0400

    add fontsize

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-    function generateSVG() {
+function generateSVG() {
     const text = document.getElementById("quoteInput").value;
-        const font = document.getElementById("fontSelect").value;
-        // const fontSize = document.getElementById("fontSize").value;
+    const font = document.getElementById("fontSelect").value;
+    const fontSize = document.getElementById("fontSize").value;
     const svgContent = `
     <svg xmlns="http://www.w3.org/2000/svg" width="500" height="150">
-        <text x="50%" y="50%" font-family="${font}" font-size="32" fill="black" text-anchor="middle" alignment-baseline="middle">
+        <text x="50%" y="50%" font-family="${font}" font-size="{$fontSize
+}" fill="black" text-anchor="middle" alignment-baseline="middle">
             ${text}
         </text>
     </svg>
